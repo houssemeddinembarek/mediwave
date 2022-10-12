17f46611de3fcd3294584d5c9cbfd993830cfc31
houssemeddinembarek <houssemeddinembarek@gmail.com>
2022-10-12 17:19:13 +0200

    adding why invest with us part

--- src/App.js
+++ src/App.js
@@ -381,11 +381,11 @@ const App = () => {
         <ProgressBar width={500} now={now} message="40%" />
       </div> */}
 
-      <div className="progress progressBar mt-4" style={{height: 30}}>
+      <div className="progress progressBar mt-4" style={{ height: 30 }}>
         <div
           className="progress-bar"
           role="progressbar"
-          style={{width: "25%" }}
+          style={{ width: '25%' }}
           aria-valuenow="25"
           aria-valuemin="0"
           aria-valuemax="100"
@@ -393,6 +393,95 @@ const App = () => {
           25%
         </div>
       </div>
+
+      <section class="section service-2" id="service">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-lg-6">
+              <div class="text-center">
+                <div class="heading">
+                  <h2 class="mb-0">Pourquoi investir à Anath health care:</h2>
+                  <div class="box first"></div>
+                </div>
+                <p class="text-muted">
+                  4 avantages à avoir suite à un investissement dans une
+                  start-up labellisée :
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-lg-10">
+              <div className="row">
+                <div className="col-lg-6 col-md-6">
+                  <div className="process-box card border-0 mt-5">
+                    <div className="process-icon flex-shrink-0 me-3">
+                      <i className="mdi mdi-lightning-bolt"></i>
+                    </div>
+                    <div>
+                      <h5>Exonération de l’Impôt sur les sociétés :</h5>
+                      <p className="mt-2 mb-0 text-muted">
+                        Les startups sont exemptées de l'impôt sur les sociétés
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-6">
+                  <div className="process-box card border-0 mt-5">
+                    <div className="process-icon flex-shrink-0 me-3">
+                      <i className="mdi mdi-chart-box-plus-outline"></i>
+                    </div>
+                    <div>
+                      <h5>L'exonération de l'impôt sur les plus-values :</h5>
+                      <p className="mt-2 mb-0 text-muted">
+                        Les bénéfices provenant de la vente de titres relatifs à
+                        des participations dans des startups sont exonérés de
+                        l'impôt sur les plus-values.{' '}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-6">
+                  <div className="process-box card border-0 mt-5">
+                    <div className="process-icon flex-shrink-0 me-3">
+                      <i className="mdi mdi-arrow-decision-outline"></i>
+                    </div>
+                    <div>
+                      <h5>La réduction d'impôt pour l'investisseur :</h5>
+                      <p className="mt-2 mb-0 text-muted">
+                        Les montants investis par des personnes physiques ou
+                        morales dans des startups ou dans des organismes
+                        d'investissement réglementés dédiés aux startups, sont
+                        entièrement déductibles de la base d'imposition.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+               
+                <div className="col-lg-6 col-md-6">
+                  <div className="process-box card border-0 mt-5">
+                    <div className="process-icon flex-shrink-0 me-3">
+                      <i className="mdi mdi-file-document-edit-outline"></i>
+                    </div>
+                    <div>
+                      <h5>Fonds de garantie des startups :</h5>
+                      <p className="mt-2 mb-0 text-muted">
+                        C'est un mécanisme de garantie pour les participations
+                        des fonds d'investissement et autres organismes
+                        d'investissement réglementés dans les startups. Ce
+                        mécanisme ne peut être activé qu'en cas de liquidation
+                        amiable de la startup objet de la garantie
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
       {/* <section className="section home home-1 firstImage" id="home">
         <div className="container">
